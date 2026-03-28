8072cad61b426b76233cbc95f701b03a6fcfeca8
Mael Seewald <mael.seewald@adnovum.ch>
2026-03-28 15:14:18 +0100

    fix env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,15 +2,18 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+  const env = loadEnv(mode, '../');
 
   return {
+    envDir: '../',
     server: {
       port: Number(env.VITE_PORT),
       proxy: {
-        '/api': env.VITE_BACKEND_URL,
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+        },
       },
     },
     plugins: [react(), tailwindcss()],
